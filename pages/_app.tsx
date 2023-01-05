940617c2e3e60d4cca7a8a0cdfa6d5f2a58a3df5
Alan Oliveira <alan@mykargo.com>
2023-01-05 01:07:53 -0300

    feat(pages): adding basic page structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,46 @@
+import { extendTheme, StyleFunctionProps } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const extendedTheme = extendTheme({
+  fonts: {
+    heading: `Anybody, sans-serif`,
+    body: `PT Sans, sans-serif`,
+  },
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        h: '100%',
+        backgroundColor: '#0C0C13',
+      },
+      html: {
+        h: '100%',
+      },
+      '#__next': {
+        h: '100%',
+      },
+    }),
+  },
+});
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <ChakraProvider theme={extendedTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>,
+  );
+};
+
+export default App;
---
+++ pages/home/home.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Rocket science</title>
+      </Head>
+    </>
+  );
+};
